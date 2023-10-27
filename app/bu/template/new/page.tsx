3dd54c7d9bef26fb86bdf71f709df34ffebd428c
QuangMinh94 <quangminhdinh38@gmail.com>
2023-10-27 15:32:46 +0700

    Add service

--- app/bu/template/new/page.tsx
+++ app/bu/template/new/page.tsx
@@ -1,10 +1,29 @@
 import ProviderTemplate from "@/components/context/providerTemplate"
+import axios from "axios"
+import { OptionProps } from "../[id]/page"
 import NewTemplateWrapper from "../_components/NewTemplateWrapper"
 
-const NewTemplate = () => {
+const NewTemplate = async () => {
+    const response = await axios.post(process.env.NEXT_PUBLIC_EFORM_LIST!, {
+        repository: "Dịch vụ tài khoản"
+    })
+    const res_1 = response.data as {
+        name: string
+        repository: string
+        serverPath: string
+    }[]
+    const _option: OptionProps[] = []
+    res_1.forEach((resChild) => {
+        _option.push({
+            id: resChild.repository + resChild.name,
+            name: resChild.name,
+            checkBox: false,
+            type: resChild.repository
+        })
+    })
     return (
         <ProviderTemplate>
-            <NewTemplateWrapper data={[]} />
+            <NewTemplateWrapper listLeft={_option} data={[]} />
         </ProviderTemplate>
     )
 }
