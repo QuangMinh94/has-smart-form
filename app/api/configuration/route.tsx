539938b04ebb9a6e5d9f19f82dffd592bc191d7f
QuangMinh94 <quangminhdinh38@gmail.com>
2023-11-17 09:49:06 +0700

    Build docker

--- app/api/configuration/route.tsx
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server"
-
-export function GET() {
-    return NextResponse.json({}, { status: 200 })
-}
-
-export function POST(nextRequest: NextRequest) {
-    return NextResponse.json({}, { status: 200 })
-}
