d52a85c7be4eb157e836b98bd59c10ae801c7f5b
QuangMinh94 <quangminhdinh38@gmail.com>
2023-11-20 17:24:33 +0700

    Add loading for signin

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,4 +1,25 @@
-import SigninForm from "./signInForm"
+import { Spin } from "antd"
+import dynamic from "next/dynamic"
+const SigninForm = dynamic(() => import("./signInForm"), {
+    loading: () => (
+        <div
+            style={{
+                backgroundImage: `url(/img/background.png)`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                //backgroundColor: "rgba(228, 240, 244, 1)",
+                height: "100vh",
+                width: "100vw",
+                justifyContent: "center",
+                alignItems: "center"
+                //zIndex: 1
+            }}
+        >
+            <Spin />
+        </div>
+    ),
+    ssr: false
+})
 
 const SigninPage = () => {
     return <SigninForm />
